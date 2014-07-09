91a0f5c286b14d196c3772217504b6fee9b8a8c0
mvfad91 <mvfad@hotmail.com>
2014-07-09 05:11:39 -0300

    Update RotasUsuario.js

--- backend/rotas/RotasUsuario.js
+++ backend/rotas/RotasUsuario.js
@@ -5,19 +5,15 @@ module.exports = function(app){
     app.post('/newUser', usuarioCtrl.insert, function(req, res){
         
     });
-
     app.get('/usuario/:u', usuarioCtrl.getById, function(req, res){
 
     });
-
     app.post('/usuarios', usuarioCtrl.getAll, function(req, res){
 
     });
-
     app.post('/buscarUsuario/nome', usuarioCtrl.getAll, function(req, res){
 
     });
-
     app.post('/addAmigo', usuarioCtrl.addAmigo, function(req, res){
 
     });
@@ -27,27 +23,21 @@ module.exports = function(app){
     app.post('/getSeguidores', usuarioCtrl.getSeguidores, function(req, res){
 
     });
-
     app.get('/totalSeguindo/:u', usuarioCtrl.getTotalSeguindo, function(req, res){
 
     });
-
     app.post('/alterar/imagem', usuarioCtrl.update, function(req, res){
 
     });
-
     app.put('/alterar/usuario', usuarioCtrl.updateNames, function(req, res){
 
     });
-
     app.put('/alterar/email', usuarioCtrl.updateEmail, function(req, res){
 
     });
-
     app.put('/alterar/senha', usuarioCtrl.updateSenha, function(req, res){
 
     });
-
     app.post('/remover', usuarioCtrl.remove, function(req, res){
 
     });
